09ff7553c88588584787f5f4c66def09b1c2a85d
AiroGriefwind <130520211+AiroGriefwind@users.noreply.github.com>
2025-10-09 15:37:38 +0800

    Debug log for tranform metadata line

--- utils/document_utils.py
+++ utils/document_utils.py
@@ -152,6 +152,9 @@ def transform_metadata_line(metadata_text, next_paragraph_text, monday_mode=Fals
             date_match = match.group(0)
             break
     
+    #Debug log for all variables involved in Monday mode
+    st.write(f"Debug: metadata_text='{metadata_text}', next_paragraph_text='{next_paragraph_text}', monday_mode={monday_mode}, sunday_date={sunday_date}, date_match={date_match}, short_media_name='{short_media_name}', page_label='{page_label}', body='{body}'")
+
     # If Monday mode is ON and date matches target Sunday, prefix
     if monday_mode and sunday_date and date_match == sunday_date:
         transformed = f"{sunday_date} {short_media_name} {page_label}：{body}".replace('  ', ' ').strip()
